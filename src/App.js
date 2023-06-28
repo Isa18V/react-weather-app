02ed6d615805034f5b8d1a5309a723963c367b4d
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-06-28 13:28:45 +0200

    added weather file && bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.delac.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Isabel voican
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Isa18V/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-react-app-isav18.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
