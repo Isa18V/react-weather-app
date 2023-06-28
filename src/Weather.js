6af02a078c5d09fa4cb8f0d7866999426c78a3e7
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-06-28 15:17:16 +0200

    made weather html structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,29 @@
 import React from "react";
+import "./weather.css";
 
 export default function Weather() {
-  return <div>hello</div>;
+  return (
+    <div className="weather">
+      <div className="destination-description">
+        <h1>rome</h1>
+        <p>woensdag 14:00</p>
+        <p>Overwegend bewolkt</p>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Overwegend bewolkt"
+          ></img>{" "}
+          21 C°
+        </div>
+        <div className="col-6">
+          <p>Neerslagkans: 1%</p>
+          <p> Luchtvochtigheid: 56%</p>
+          <p>Wind: 11 km/h</p>
+        </div>
+      </div>
+    </div>
+  );
 }
