7974954bc2a692fddcb8cfc23e325cef3acc9bda
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-06-29 12:07:44 +0200

    Display the current temperature and destination details

--- src/Weather.js
+++ src/Weather.js
@@ -1,115 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div class="container-form">
-        <form>
-          <label>
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+  const [temperature, setTemperature] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-      <div className="destination-description">
-        <h1>Rome</h1>
-        <ul>
-          <li>woensdag 14:00</li>
-          <li>Overwegend bewolkt</li>
-        </ul>
-      </div>
+  function HandleResponse(response) {
+    setTemperature(response.data.temperature.current);
 
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-            alt="Overwegend bewolkt"
-          ></img>{" "}
-          21 C°
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Neerslagkans: 1%</li>
-            <li>Luchtvochtigheid: 56%</li>
-            <li>Wind: 11 km/</li>
-          </ul>
-        </div>
-      </div>
+    setLoading(true);
+  }
 
-      {/* here is the row with all the days */}
-      <div className="row">
-        <div className="col-3">
+  if (loading) {
+    return (
+      <div className="Weather">
+        <div className="container-form">
+          <form>
+            <label>
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <div className="destination-description">
+          <h1>Rome</h1>
           <ul>
-            <li>monday</li>
-            <li>
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                alt="Overwegend bewolkt"
-              ></img>
-            </li>
-            <li>
-              <div className="row">
-                <div className="col-6">12°</div>
-                <div className="col-6">20°</div>
-              </div>
-            </li>
+            <li>woensdag 14:00</li>
+            <li>Overwegend bewolkt</li>
           </ul>
         </div>
-        <div className="col-3">
-          <ul>
-            <li>teusday</li>
-            <li>
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                alt="Overwegend bewolkt"
-              ></img>
-            </li>
-            <li>
-              <div className="row">
-                <div className="col-6">12°</div>
-                <div className="col-6">20°</div>
-              </div>
-            </li>
-          </ul>
+
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Overwegend bewolkt"
+            ></img>
+            {Math.round(temperature)} C°
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Neerslagkans: 1%</li>
+              <li>Luchtvochtigheid: 56%</li>
+              <li>Wind: 11 km/</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-3">
-          <ul>
-            <li>wensday</li>
-            <li>
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                alt="Overwegend bewolkt"
-              ></img>
-            </li>
-            <li>
-              <div className="row">
-                <div className="col-6">12°</div>
-                <div className="col-6">20°</div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="col-3">
-          <ul>
-            <li>thusrday</li>
-            <li>
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                alt="Overwegend bewolkt"
-              ></img>
-            </li>
-            <li>
-              <div className="row">
-                <div className="col-6">12°</div>
-                <div className="col-6">20°</div>
-              </div>
-            </li>
-          </ul>
+
+        {/* here is the row with all the days */}
+        <div className="row">
+          <div className="col-3">
+            <ul>
+              <li>monday</li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Overwegend bewolkt"
+                ></img>
+              </li>
+              <li>
+                <div className="row">
+                  <div className="col-6">12°</div>
+                  <div className="col-6">20°</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <ul>
+              <li>teusday</li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Overwegend bewolkt"
+                ></img>
+              </li>
+              <li>
+                <div className="row">
+                  <div className="col-6">12°</div>
+                  <div className="col-6">20°</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <ul>
+              <li>wensday</li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Overwegend bewolkt"
+                ></img>
+              </li>
+              <li>
+                <div className="row">
+                  <div className="col-6">12°</div>
+                  <div className="col-6">20°</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <ul>
+              <li>thusrday</li>
+              <li>
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Overwegend bewolkt"
+                ></img>
+              </li>
+              <li>
+                <div className="row">
+                  <div className="col-6">12°</div>
+                  <div className="col-6">20°</div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ecb1e604034014f9c94ecdtbo8ae777f";
+    let query = "London";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(HandleResponse);
+    return "Loading ...";
+  }
 }
