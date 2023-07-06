7592ddb1f071c2806170a787fc67b0b0bb22fce1
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-06 19:53:43 +0200

    added new component for the forcast days

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
+
 import "./Weatherforecast.css";
 import axios from "axios";
 
 export default function WeatherForcast(props) {
-  function HandleResponse(response) {
-    console.log(response.data);
-  }
-  const apikey = "ecb1e604034014f9c94ecdtbo8ae777f";
-  let latitude = props.coordinates.latitude;
-  let longitude = props.coordinates.longitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apikey}&units=metric`;
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  axios.get(apiUrl).then(HandleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <div>icon</div>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span>
-            <span className="WeatherForecast-temperature-min">10°</span>
+  function HandleResponse(response) {
+    setLoaded(true);
+    setForecast(response.data.daily[0]);
+  }
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay date={forecast} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apikey = "ecb1e604034014f9c94ecdtbo8ae777f";
+    let latitude = props.coordinates.latitude;
+    let longitude = props.coordinates.longitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apikey}&units=metric`;
+
+    axios.get(apiUrl).then(HandleResponse);
+  }
 }
