e8ca1eefd2014c8d642565da72f5b9d1f92e4966
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-11 18:07:50 +0200

    updated weatherForcast
    api request

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
-import "./Weatherforecast.css";
-import axios from "axios";
-
-export default function WeatherForcast(props) {
+export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
@@ -16,34 +14,31 @@ export default function WeatherForcast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  function load() {
-    let apiKey = `9eca7aac0b071aa16e3cb063adba0785`;
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map((dailyForecast, index) => {
+          {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
-            } else {
-              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    load();
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return null;
   }
 }
