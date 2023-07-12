3f4e95962527aae6ed2c70549d3e98fc3c248bf3
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-12 12:19:26 +0200

    made weatherIcon component
    imporeted react and made a codemapping for a few icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,10 +1,22 @@
 import React from "react";
-import img1 from "./images/clearSky.png";
+import clearSkyImg from "./images/clearSky.png";
+import fewCloudsImg from "./images/fewClouds.png";
+// Import other images for different weather conditions
 
 export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": clearSkyImg,
+    "01n": "CLEAR_NIGHT",
+    "02d": fewCloudsImg,
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    // Add more weather code mappings with imported images for different weather conditions
+  };
+
+  const imgSource = codeMapping[props.data] || clearSkyImg;
+
   return (
     <div>
-      <img src={img1} alt="hello"></img>
+      <img src={imgSource} style={{ width: "80px", height: "80px" }} alt="" />
     </div>
   );
 }
